0cdc5280bdbd0f28c1b25eff4f55bcd794f60605
aaradhyan <Aaradhya.Nachankar@tapasi.com>
2024-09-27 10:23:04 +0530

    By me new Item

---
+++ JenkinsGithubIntegration/src/com/cjc/JGPro.java
@@ -0,0 +1,27 @@
+package com.cjc;
+
+public class JGPro {
+public static void main(String[] args) {
+	System.out.println("Its main method");
+}
+public void m5()
+{
+	System.out.println("m5 method");
+}
+public void m4()
+{
+	System.out.println("m4 method");
+}
+public void m3()
+{
+	System.out.println("m3 method");
+}
+public void m2()
+{
+	System.out.println("m2 method");
+}
+public void m1()
+{
+	System.out.println("m1 method");
+}
+}
